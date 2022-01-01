a9c758cdc77501e36d32181cff31bb6211e6d830
Charlie <me@charliemoomoo.cloudns.asia>
2022-01-01 19:39:51 +0800

    Update button.js

--- button.js
+++ button.js
@@ -1,6 +1,6 @@
 window.onload = function (){
   const elements = document.getElementsByClassName('likecoin-button')
 for(i=0;i<elements.length;i++){
-　elements[i].innerHTML = '<iframe frameborder="0" height="200px" src="https://button.like.co/in/embed/'+elements[i].innerHTML+'/button?referrer='+window.location.href+'&type=omit">';
+　elements[i].innerHTML = '<iframe frameborder="0" height="200px" src="https://button.like.co/in/embed/'+elements[i].innerHTML.toLowerCase()+'/button?referrer='+window.location.href+'&type=omit">';
 }
 }
